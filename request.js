a1494499d3a793eb29a93b35d1d60e4eca14ed7f
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2022-11-21 23:47:20 +0100

    ajout id

--- request.js
+++ request.js
@@ -5,6 +5,7 @@ const sci_fi_url = "http://localhost:8000/api/v1/titles/?genre=Sci-fi&sort_by=-i
 const thriller_url = "http://localhost:8000/api/v1/titles/?genre=thriller&sort_by=-imdb_score&page_size=7"
 const drama_url = "http://localhost:8000/api/v1/titles/?genre=drama&sort_by=-imdb_score&page_size=7"
 
+
 function getBestMovie(url) {
     // Création de l'objet xhr
     const xhr = new XMLHttpRequest();
@@ -12,71 +13,90 @@ function getBestMovie(url) {
     xhr.onload = function() {
         if (this.readyState == 4 && this.status == 200) {
             const resObj = JSON.parse(this.responseText) // Objet json
-            const bestMovie = document.getElementById("movie-picture")
+            
             //console.log(resObj.results);
 
             for (element of resObj.results) {
-                //console.log(element.id);
-                bestMovie.innerHTML += '<img onclick="openModal('+ element.id +')" src="' + element.image_url + '" alt="" class="best-image">\n \
-        <div id="' + String(element.id) + '" class="modal">\n \
-            <div class="modal-content">\n \
-                <span class="close">×</span>\n \
-                <ul>\n \
-                    <li> Titre :  ' + element.title + '</li>\n \
-                    <li> Genre :  ' + element.genres + '</li>\n \
-                    <li> Année :  ' + element.year + '</li>\n \
-                    <li> Votes :  ' + element.votes + '</li>\n \
-                    <li> Score IMDB : ' + element.imdb_score + '</li>\n \
-                    <li> Réalisateur : ' + element.directors + '</li>\n \
-                    <li> Acteurs : ' + element.actors + '</li>\n \
-                    <li> Pays : ' + element.country + '</li>\n \
-            </div>\n \
-        </div>\n'
+                new_url = "http://localhost:8000/api/v1/titles/" + element.id
+                //console.log(new_url)
+                const result = new XMLHttpRequest;
+                result.onload = function() {
+                    const objt = JSON.parse(result.responseText);
+                    const bestMovie = document.getElementById("movie-picture");
 
-                //console.log(bestMoviePic.innerHTML);
+                    //console.log(objt.id)
 
-            } 
-        }   else if (this.status == 404) {
-                document.getElementById("best_movie").innerHTML = "Erreur 404";
+                    bestMovie.innerHTML += '<img onclick="openModal('+ objt.id +')" src="' + objt.image_url + '" alt="" class="best-image">\n \
+                    <div id="' + String(objt.id) + '" class="modal">\n \
+                        <div class="modal-content">\n \
+                            <span class="close">×</span>\n \
+                            <ul>\n \
+                                <li> Titre :  ' + objt.original_title + '</li>\n \
+                                <li> Genre :  ' + objt.genres + '</li>\n \
+                                <li> Année de sortie :  ' + objt.year + '</li>\n \
+                                <li> Notes :  ' + objt.avg_vote + '</li>\n \
+                                <li> Score IMDB : ' + objt.imdb_score + '</li>\n \
+                                <li> Réalisateur : ' + objt.directors + '</li>\n \
+                                <li> Acteurs : ' + objt.actors + '</li>\n \
+                                <li> Durée : ' + objt.duration + 'min</li>\n \
+                                <li> Pays : ' + objt.countries + '</li>\n \
+                                <li> Box office : ' + objt.worldwide_gross_income + '</li>\n \
+                                <li> Synopsis : ' + objt.description + '</li>\n \
+                        </div>\n \
+                    </div>\n'
+                }
+                result.open("GET", new_url, true);
+                result.send();
+            }
         }
-            else {
-                document.getElementById("best_movie").innerHTML = "<img src='images/loading_mini.gif'>";
-        }
-    }   
-        // Spécifier la ressource sur le serveur 
-        xhr.open("GET", url, true);
-        xhr.send();
     }
+    xhr.open("GET", url, true);
+    xhr.send();
+    
+}
 
 function getBestMovieCategory(url, category) {
     const xhr = new XMLHttpRequest();
     xhr.onload = function() {
         if (this.readyState == 4 && this.status == 200) {
             const resObj = JSON.parse(this.responseText);
-            const bestCategory = document.getElementById(category);
-            //console.log(bestCategory);
+            
 
-            var resultObj = resObj["results"]
+            var resultObj = resObj["results"];
+            //console.log(resultObj)
 
-            for (i = 0; i < resultObj.length; i++) {
-                //console.log(resultObj[i]["title"]);
-                bestCategory.innerHTML +='<img onclick="openModal(' + resultObj[i]['id'] + ')" src="' + resultObj[i]['image_url'] + '">\n\
-                <div id="' + String(resultObj[i]['id']) + '" class="modal">\n\
-                    <div class="modal-content">\n\
-                        <span class="close">×</span>\n\
-                        <ul>\n\
-                            <li> Titre :  ' + resultObj[i]["title"] + '</li>\n\
-                            <li> Genre :  ' + resultObj[i]["genres"] + '</li>\n\
-                            <li> Année :  ' + resultObj[i]["year"] + '</li>\n\
-                            <li> Votes :  ' + resultObj[i]["votes"] + '</li>\n\
-                            <li> Score IMDB : ' + resultObj[i]["imdb_score"] + '</li>\n\
-                            <li> Réalisateur : ' + resultObj[i]["directors"] + '</li>\n\
-                            <li> Acteurs : ' + resultObj[i]["actors"] + '</li>\n\
-                            <li> Pays : ' + resultObj[i]["country"] + '</li>\n\
-                    </div>\n\
-                </div>\n'
+        for (i = 0; i < resultObj.length; i++) {
+                new_url = "http://localhost:8000/api/v1/titles/" + resultObj[i]["id"];
+                //console.log(new_url);
+                const result = new XMLHttpRequest;
+                result.onload = function() {
+                    const objt = JSON.parse(result.responseText);
+                    const bestCategory = document.getElementById(category);   
+
+                    bestCategory.innerHTML +='<img onclick="openModal(' + objt.id + ')" src="' + objt.image_url + '">\n\
+                    <div id="' + String(objt.id) + '" class="modal">\n\
+                        <div class="modal-content">\n\
+                            <span class="close">×</span>\n\
+                            <ul>\n\
+                            <li> Titre :  ' + objt.original_title + '</li>\n \
+                            <li> Genre :  ' + objt.genres + '</li>\n \
+                            <li> Année de sortie :  ' + objt.year + '</li>\n \
+                            <li> Notes :  ' + objt.avg_vote + '</li>\n \
+                            <li> Score IMDB : ' + objt.imdb_score + '</li>\n \
+                            <li> Réalisateur : ' + objt.directors + '</li>\n \
+                            <li> Acteurs : ' + objt.actors + '</li>\n \
+                            <li> Durée : ' + objt.duration + 'min</li>\n \
+                            <li> Pays : ' + objt.countries + '</li>\n \
+                            <li> Box office : ' + objt.worldwide_gross_income + '</li>\n \
+                            <li> Synopsis : ' + objt.description + '</li>\n \
+                        </div>\n\
+                    </div>\n'
+                }
+                result.open("GET", new_url, true);
+                result.send();
+                
             }        
-                console.log(bestCategory.innerHTML)
+                //console.log(bestCategory.innerHTML)
 
         } else if (this.status == 404) {
             document.getElementById("best_movie").innerHTML = "Erreur 404";
@@ -96,3 +116,4 @@ getBestMovie(best_movie_url);
 getBestMovieCategory(sci_fi_url, "Sci-fi");
 getBestMovieCategory(thriller_url, "Thriller");
 getBestMovieCategory(drama_url, "Drama");
+
