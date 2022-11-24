9526d60c3cfca0b957c4aef1dd03f58ebf463189
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2022-11-24 18:53:23 +0100

    Finalisation

--- modal.js
+++ modal.js
@@ -1,37 +1,56 @@
 function openModal(movieId) {
 
-  var modal = document.getElementById(movieId);
-  var btn1 = document.getElementById("movie-picture");
-  var btn2 = document.getElementById("Sci-fi");
-  var btn3 = document.getElementById("Thriller");
-  var btn4 = document.getElementById("Drama");
+    var modal = document.getElementById("myModal");  
+    url = "http://localhost:8000/api/v1/titles/" + String(movieId);
+
+    const mod = new XMLHttpRequest();
+    // Interroger le serveur et récupérer la réponse 
+    mod.onload = function() {
+        
+        if (this.readyState == 4 && this.status == 200) {
+            const objt = JSON.parse(this.responseText);
+
+            var modalContent = document.getElementById("content-movie");
+            //on modifie le content
+            modalContent.innerHTML = '<img src="' + objt.image_url + '"> \n\
+            <ul>\n\
+            <li> Titre :  ' + objt.original_title + '</li>\n\
+            <li> Genre :  ' + objt.genres + '</li>\n\
+            <li> Année de sortie :  ' + objt.year + '</li>\n\
+            <li> Notes :  ' + objt.avg_vote + '</li>\n\
+            <li> Score IMDB : ' + objt.imdb_score + '</li>\n\
+            <li> Réalisateur : ' + objt.directors + '</li>\n\
+            <li> Acteurs : ' + objt.actors + '</li>\n\
+            <li> Durée : ' + objt.duration + 'min</li>\n\
+            <li> Pays : ' + objt.countries + '</li>\n\
+            <li> Box office : $' + objt.worldwide_gross_income + '</li>\n\
+            <li> Synopsis : ' + objt.description + '</li>\n\
+                </ul>';
+            //on display le modal 
+            modal.style.display = "block";
+
+            //on gere la fermeture du modal   le bouton dois porter l id close-btn
+            
+        }
   
-  btn1.onclick = function() {
-    modal.style.display = "block";
-  }
+    } 
 
-  btn2.onclick = function() {
-    modal.style.display = "block";
-  }
 
-  btn3.onclick = function() {
-    modal.style.display = "block";
-  }
-
-  btn4.onclick = function() {
-    modal.style.display = "block";
-  }
-
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    mod.open("GET", url, true);
+    mod.send();
+    
+    var span = document.getElementById("close-btn");
+  
+    span.onclick = function() {
+        modal.style.display = "none";
     }
-  }
+    
+    window.onclick = function(event) {
+        if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
+    
 
-  var span = document.getElementById("close-btn");
-
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
 }
-
+  
--- request.js
+++ request.js
@@ -1,6 +1,7 @@
 
 
 const best_movie_url = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=1"
+const best_movies_url = "http://localhost:8000/api/v1/titles/?page_size=7&sort_by=-imdb_score"
 const sci_fi_url = "http://localhost:8000/api/v1/titles/?genre=Sci-fi&sort_by=-imdb_score&page_size=7"
 const thriller_url = "http://localhost:8000/api/v1/titles/?genre=thriller&sort_by=-imdb_score&page_size=7"
 const drama_url = "http://localhost:8000/api/v1/titles/?genre=drama&sort_by=-imdb_score&page_size=7"
@@ -26,7 +27,7 @@ function getBestMovie(url) {
 
                     //console.log(objt)
 
-                    bestMovie.innerHTML += '<img onclick="openModal(' + String(objt.id) + ')" src="' + objt.image_url + '">'
+                    bestMovie.innerHTML = '<img onclick="openModal(' + String(objt.id) + ')" src="' + objt.image_url + '">'
                     
                     //console.log(objt.original_title)
                     //console.log(bestMovie.innerHTML)
@@ -35,10 +36,15 @@ function getBestMovie(url) {
                 result.send();
             }
         }
+        else if (this.status == 404) {
+            document.getElementById("best_movie").innerHTML = "Erreur 404";
+        }
+        else {
+            document.getElementById("best_movie").innerHTML = "<img src='images/loading_mini.gif'>";
+        }
     }
     xhr.open("GET", url, true);
     xhr.send();
-    
 }
 
 function getBestMovieCategory(url, category) {
@@ -49,31 +55,17 @@ function getBestMovieCategory(url, category) {
             
 
             var resultObj = resObj["results"];
-            //console.log(resultObj)
+            //console.log("RESULTAT", resultObj)
 
+        const bestCategory = document.getElementById(category); 
         for (i = 0; i < resultObj.length; i++) {
-                new_url = "http://localhost:8000/api/v1/titles/" + resultObj[i]["id"];
-                //console.log(new_url);
-                const result = new XMLHttpRequest;
-                result.onload = function() {
-                    const objt = JSON.parse(result.responseText);
-                    const bestCategory = document.getElementById(category);   
-
-                    bestCategory.innerHTML ='<img onclick="openModal(' + String(objt.id) + ')" src="' + objt.image_url + '">'
+            const objt = resultObj[i];
+            bestCategory.innerHTML +='<img onclick="openModal(' + String(objt.id) + ')" src="' + objt.image_url + '">'
                     
                     //console.log(objt.original_title); 
                     //console.log(bestCategory.innerHTML);
-                }
 
-                    
-                
-
-                result.open("GET", new_url, true);
-                result.send();
-                
             }   
-            
-
         } else if (this.status == 404) {
             document.getElementById("best_movie").innerHTML = "Erreur 404";
         }
@@ -87,9 +79,10 @@ function getBestMovieCategory(url, category) {
 }
 
 
-
 getBestMovie(best_movie_url);
+getBestMovieCategory(best_movies_url, "all-category");
 getBestMovieCategory(sci_fi_url, "Sci-fi");
 getBestMovieCategory(thriller_url, "Thriller");
 getBestMovieCategory(drama_url, "Drama");
 
+location.reload
